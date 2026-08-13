f8996a12db8add13895e2a4317c187cc12d50645
prasanna-sahu <prasanna.sahu@mindnerves>
2026-08-13 10:41:20 +0530

    Renamed the main class

--- src/main/java/com/heapsteep/SpringSecurityDemoWithCredentialsApplication.java
+++ src/main/java/com/heapsteep/SpringSecurityDemoWithCredentialsApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringSecurityDemoUsingCredsNJwtApplication {
+public class SpringSecurityDemoWithCredentialsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringSecurityDemoUsingCredsNJwtApplication.class, args);
+		SpringApplication.run(SpringSecurityDemoWithCredentialsApplication.class, args);
 		System.out.println("Sai Ram...");
 	}
 
